eed80242d1fd5b5f40893da63c3d9bc104fcebe9
RiadHossain43 <riadhossain7464@gmail.com>
2023-06-29 00:44:52 +0600

    redis code updae

--- src/emailvalidations/services/queues/validation.queue.js
+++ src/emailvalidations/services/queues/validation.queue.js
@@ -1,10 +1,8 @@
 const Bull = require("bull");
 const validationprocess = require("./validation.process");
 const { logger } = require("../../../common/helper");
-logger.info(process.env.REDIS_URL)
-const queue = new Bull("email-validation", {
-  redis: process.env.REDIS_URL
-});
+logger.info(process.env.REDIS_URL);
+const queue = new Bull("email-validation", process.env.REDIS_URL);
 queue.process(validationprocess.consume);
 exports.produce = (data) => {
   queue.add(data, {});
@@ -14,9 +12,9 @@ queue.on("completed", (job) => {
   logger.info("cleaning up the completed campaign jobs.");
   queue.clean(0, "completed");
 });
-queue.on('error', (error) => {
+queue.on("error", (error) => {
   logger.error(error);
-})
+});
 exports.cleanCompleted = (...args) => {
   queue.clean(...args);
 };
